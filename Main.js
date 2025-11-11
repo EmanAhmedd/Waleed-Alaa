d5fc95402d60d6556c2e0d8149cc1bb8760eae3b
Eman Ahmed <Eman.ahmedd183@gmail.com>
2025-11-11 15:51:21 +0200

    changessssssssss

--- Main.js
+++ Main.js
@@ -9,10 +9,14 @@
       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      document.getElementById("days").textContent = days ;
-      document.getElementById("hours").textContent = hours;
-      document.getElementById("minutes").textContent = minutes ;
-      document.getElementById("seconds").textContent = seconds ;
+      // document.getElementById("days").textContent = days ;
+      // document.getElementById("hours").textContent = hours;
+      // document.getElementById("minutes").textContent = minutes ;
+      // document.getElementById("seconds").textContent = seconds ;
+      document.getElementById("days").textContent = days + "d";
+      document.getElementById("hours").textContent = hours + "h";
+      document.getElementById("minutes").textContent = minutes + "m";
+      document.getElementById("seconds").textContent = seconds + "s";
     }, 1000);
 
     function DirectionPage() {
